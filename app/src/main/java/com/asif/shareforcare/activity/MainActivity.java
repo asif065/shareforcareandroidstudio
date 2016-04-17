c4efe8887d20cfd9a0faa5a843810a1fbcebe95a
asif065 <shawon065@gmail.com>
2016-04-17 13:01:23 +0600

    latest but not location update

--- app/src/main/java/com/asif/shareforcare/activity/MainActivity.java
+++ app/src/main/java/com/asif/shareforcare/activity/MainActivity.java
@@ -6,6 +6,7 @@ import com.asif.shareforcare.R;
 import com.asif.shareforcare.helper.SQLiteHandler;
 import com.asif.shareforcare.helper.SessionManager;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -15,6 +16,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -24,16 +26,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements
-		ConnectionCallbacks, OnConnectionFailedListener {
+		ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
 	private TextView txtName;
 	private TextView txtEmail;
+	private TextView txtLatitude;
+	private TextView txtLongitude;
 	private Button btnLogout;
+	private double dblLatitude;
+	private double dblLongitude;
 
 	private SQLiteHandler db;
 	private SessionManager session;
 
 	protected static final String TAG = "MainActivity";
+
+	/**
+	 * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+	 */
+	public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+	/**
+	 * The fastest rate for active location updates. Exact. Updates will never be more frequent
+	 * than this value.
+	 */
+	public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+			UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+	// Keys for storing activity state in the Bundle.
+	protected final static String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+	protected final static String LOCATION_KEY = "location-key";
+	protected final static String LAST_UPDATED_TIME_STRING_KEY = "last-updated-time-string-key";
 
 	/**
 	 * Provides the entry point to Google Play services.
@@ -45,6 +68,16 @@ public class MainActivity extends Activity implements
 	 * Represents a geographical location.
 	 */
 	protected Location mLastLocation;
+
+	/**
+	 * Stores parameters for requests to the FusedLocationProviderApi.
+	 */
+	protected LocationRequest mLocationRequest;
+
+	/**
+	 * Represents a geographical location.
+	 */
+	protected Location mCurrentLocation;
 
 	protected String mLatitudeLabel;
 	protected String mLongitudeLabel;
@@ -64,6 +97,9 @@ public class MainActivity extends Activity implements
 
 		txtName = (TextView) findViewById(R.id.name);
 		txtEmail = (TextView) findViewById(R.id.email);
+		txtLatitude = (TextView) findViewById(R.id.latitude);
+		txtLongitude = (TextView) findViewById(R.id.longitude);
+
 		btnLogout = (Button) findViewById(R.id.btnLogout);
 
 		// SqLite database handler
@@ -85,6 +121,7 @@ public class MainActivity extends Activity implements
 		// Displaying the user details on the screen
 		txtName.setText(name);
 		txtEmail.setText(email);
+
 
 		// Logout button click event
 		btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -109,6 +146,36 @@ public class MainActivity extends Activity implements
 				.addApi(LocationServices.API)
 				.build();
 		Log.i(TAG, "Build");
+		createLocationRequest();
+	}
+
+	/**
+	 * Sets up the location request. Android has two location request settings:
+	 * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+	 * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+	 * the AndroidManifest.xml.
+	 * <p/>
+	 * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+	 * interval (5 seconds), the Fused Location Provider API returns location updates that are
+	 * accurate to within a few feet.
+	 * <p/>
+	 * These settings are appropriate for mapping applications that show real-time location
+	 * updates.
+	 */
+	protected void createLocationRequest() {
+		mLocationRequest = new LocationRequest();
+
+		// Sets the desired interval for active location updates. This interval is
+		// inexact. You may not receive updates at all if no location sources are available, or
+		// you may receive them slower than requested. You may also receive updates faster than
+		// requested if other applications are requesting location at a faster interval.
+		mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+		// Sets the fastest rate for active location updates. This interval is exact, and your
+		// application will never receive updates faster than this value.
+		mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 	}
 
 	@Override
@@ -116,7 +183,7 @@ public class MainActivity extends Activity implements
 		super.onStart();
 		mGoogleApiClient.connect();
 
-		Toast.makeText(this, "OmStart", Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "OnStart", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
@@ -140,6 +207,10 @@ public class MainActivity extends Activity implements
 			mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
 			if (mLastLocation != null) {
+				dblLatitude = mLastLocation.getLatitude();
+				dblLongitude = mLastLocation.getLongitude();
+				txtLatitude.setText(Double.toString(dblLatitude));
+				txtLongitude.setText(Double.toString(dblLongitude));
 				Toast.makeText(this, String.format("%s, %s: %f, %f", mLatitudeLabel, mLongitudeLabel, mLastLocation.getLatitude(), mLastLocation.getLongitude()), Toast.LENGTH_LONG).show();
 			} else {
 				Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
@@ -174,4 +245,23 @@ public class MainActivity extends Activity implements
 		finish();
 	}
 
+	@Override
+	public void onLocationChanged(Location location) {
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+
+	}
 }
