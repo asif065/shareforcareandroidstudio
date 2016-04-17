bbf84111e79b47bc6226212aa6272f820b91107e
asif065 <shawon065@gmail.com>
2016-04-17 14:17:32 +0600

    latest

--- app/src/main/java/com/asif/shareforcare/activity/MainActivity.java
+++ app/src/main/java/com/asif/shareforcare/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.asif.shareforcare.activity;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 import com.asif.shareforcare.R;
@@ -11,12 +13,12 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -35,6 +37,8 @@ public class MainActivity extends Activity implements
 	private Button btnLogout;
 	private double dblLatitude;
 	private double dblLongitude;
+	protected Button mStartUpdatesButton;
+	protected Button mStopUpdatesButton;
 
 	private SQLiteHandler db;
 	private SessionManager session;
@@ -79,8 +83,21 @@ public class MainActivity extends Activity implements
 	 */
 	protected Location mCurrentLocation;
 
+	// Labels.
 	protected String mLatitudeLabel;
 	protected String mLongitudeLabel;
+	protected String mLastUpdateTimeLabel;
+
+	/**
+	 * Tracks the status of the location updates request. Value changes when the user presses the
+	 * Start Updates and Stop Updates buttons.
+	 */
+	protected Boolean mRequestingLocationUpdates;
+
+	/**
+	 * Time when the location was updated represented as a String.
+	 */
+	protected String mLastUpdateTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -89,11 +106,7 @@ public class MainActivity extends Activity implements
 
 		mLatitudeLabel = getResources().getString(R.string.latitude_label);
 		mLongitudeLabel = getResources().getString(R.string.longitude_label);
-
-
-		Log.i(TAG, "Calling Build");
-
-		buildGoogleApiClient();
+		mLastUpdateTimeLabel = getResources().getString(R.string.last_update_time_label);
 
 		txtName = (TextView) findViewById(R.id.name);
 		txtEmail = (TextView) findViewById(R.id.email);
@@ -101,6 +114,8 @@ public class MainActivity extends Activity implements
 		txtLongitude = (TextView) findViewById(R.id.longitude);
 
 		btnLogout = (Button) findViewById(R.id.btnLogout);
+		mStartUpdatesButton = (Button) findViewById(R.id.start_updates_button);
+		mStopUpdatesButton = (Button) findViewById(R.id.stop_updates_button);
 
 		// SqLite database handler
 		db = new SQLiteHandler(getApplicationContext());
@@ -132,7 +147,121 @@ public class MainActivity extends Activity implements
 			}
 		});
 
+		mRequestingLocationUpdates = false;
+		mLastUpdateTime = "";
 
+		// Update values using data stored in the Bundle.
+		updateValuesFromBundle(savedInstanceState);
+
+		Log.i(TAG, "Calling Build");
+
+		buildGoogleApiClient();
+
+	}
+
+	/**
+	 * Updates fields based on data stored in the bundle.
+	 *
+	 * @param savedInstanceState The activity state saved in the Bundle.
+	 */
+	private void updateValuesFromBundle(Bundle savedInstanceState) {
+		Log.i(TAG, "Updating values from bundle");
+		if (savedInstanceState != null) {
+			// Update the value of mRequestingLocationUpdates from the Bundle, and make sure that
+			// the Start Updates and Stop Updates buttons are correctly enabled or disabled.
+			if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+				mRequestingLocationUpdates = savedInstanceState.getBoolean(
+						REQUESTING_LOCATION_UPDATES_KEY);
+			}
+
+			// Update the value of mCurrentLocation from the Bundle and update the UI to show the
+			// correct latitude and longitude.
+			if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+				// Since LOCATION_KEY was found in the Bundle, we can be sure that mCurrentLocation
+				// is not null.
+				mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+			}
+
+			// Update the value of mLastUpdateTime from the Bundle and update the UI.
+			if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+				mLastUpdateTime = savedInstanceState.getString(LAST_UPDATED_TIME_STRING_KEY);
+			}
+			updateUI();
+		}
+	}
+
+
+	/**
+	 * Updates the latitude, the longitude, and the last location time in the UI.
+	 */
+	private void updateUI() {
+		txtLatitude.setText(String.format("%s: %f", mLatitudeLabel,
+				mCurrentLocation.getLatitude()));
+		txtLongitude.setText(String.format("%s: %f", mLongitudeLabel,
+				mCurrentLocation.getLongitude()));
+		//mLastUpdateTimeTextView.setText(String.format("%s: %s", mLastUpdateTimeLabel,
+		//		mLastUpdateTime));
+	}
+
+	/**
+	 * Ensures that only one button is enabled at any time. The Start Updates button is enabled
+	 * if the user is not requesting location updates. The Stop Updates button is enabled if the
+	 * user is requesting location updates.
+	 */
+	private void setButtonsEnabledState() {
+		if (mRequestingLocationUpdates) {
+			mStartUpdatesButton.setEnabled(false);
+			mStopUpdatesButton.setEnabled(true);
+		} else {
+			mStartUpdatesButton.setEnabled(true);
+			mStopUpdatesButton.setEnabled(false);
+		}
+	}
+
+	/**
+	 * Handles the Start Updates button and requests start of location updates. Does nothing if
+	 * updates have already been requested.
+	 */
+	public void startUpdatesButtonHandler(View view) {
+		if (!mRequestingLocationUpdates) {
+			mRequestingLocationUpdates = true;
+			setButtonsEnabledState();
+			startLocationUpdates();
+		}
+	}
+
+	/**
+	 * Handles the Stop Updates button, and requests removal of location updates. Does nothing if
+	 * updates were not previously requested.
+	 */
+	public void stopUpdatesButtonHandler(View view) {
+		if (mRequestingLocationUpdates) {
+			mRequestingLocationUpdates = false;
+			setButtonsEnabledState();
+			stopLocationUpdates();
+		}
+	}
+
+	/**
+	 * Requests location updates from the FusedLocationApi.
+	 */
+	protected void startLocationUpdates() {
+		// The final argument to {@code requestLocationUpdates()} is a LocationListener
+		// (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+		LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+	}
+
+	/**
+	 * Removes location updates from the FusedLocationApi.
+	 */
+	protected void stopLocationUpdates() {
+		// It is a good practice to remove location requests when the activity is in a paused or
+		// stopped state. Doing so helps battery performance and is especially
+		// recommended in applications that request frequent location updates.
+
+		// The final argument to {@code requestLocationUpdates()} is a LocationListener
+		// (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+		LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
 	}
 
 
@@ -187,6 +316,27 @@ public class MainActivity extends Activity implements
 	}
 
 	@Override
+	public void onResume() {
+		super.onResume();
+		// Within {@code onPause()}, we pause location updates, but leave the
+		// connection to GoogleApiClient intact.  Here, we resume receiving
+		// location updates if the user has requested them.
+
+		if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+			startLocationUpdates();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Stop location updates to save battery, but don't disconnect the GoogleApiClient object.
+		if (mGoogleApiClient.isConnected()) {
+			stopLocationUpdates();
+		}
+	}
+
+	@Override
 	protected void onStop() {
 		super.onStop();
 		if(mGoogleApiClient.isConnected()){
@@ -203,18 +353,42 @@ public class MainActivity extends Activity implements
 		// applications that do not require a fine-grained location and that do not need location
 		// updates. Gets the best and most recent location currently available, which may be null
 		// in rare cases when a location is not available.
-		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == getPackageManager().PERMISSION_GRANTED) {
-			mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == getPackageManager().PERMISSION_GRANTED) {
+//			mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//
+//			if (mLastLocation != null) {
+//				dblLatitude = mLastLocation.getLatitude();
+//				dblLongitude = mLastLocation.getLongitude();
+//				txtLatitude.setText(Double.toString(dblLatitude));
+//				txtLongitude.setText(Double.toString(dblLongitude));
+//				Toast.makeText(this, String.format("%s, %s: %f, %f", mLatitudeLabel, mLongitudeLabel, mLastLocation.getLatitude(), mLastLocation.getLongitude()), Toast.LENGTH_LONG).show();
+//			} else {
+//				Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+//			}
+//		}
 
-			if (mLastLocation != null) {
-				dblLatitude = mLastLocation.getLatitude();
-				dblLongitude = mLastLocation.getLongitude();
-				txtLatitude.setText(Double.toString(dblLatitude));
-				txtLongitude.setText(Double.toString(dblLongitude));
-				Toast.makeText(this, String.format("%s, %s: %f, %f", mLatitudeLabel, mLongitudeLabel, mLastLocation.getLatitude(), mLastLocation.getLongitude()), Toast.LENGTH_LONG).show();
-			} else {
-				Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
-			}
+		// If the initial location was never previously requested, we use
+		// FusedLocationApi.getLastLocation() to get it. If it was previously requested, we store
+		// its value in the Bundle and check for it in onCreate(). We
+		// do not request it again unless the user specifically requests location updates by pressing
+		// the Start Updates button.
+		//
+		// Because we cache the value of the initial location in the Bundle, it means that if the
+		// user launches the activity,
+		// moves to a new location, and then changes the device orientation, the original location
+		// is displayed as the activity is re-created.
+
+		if(mCurrentLocation == null){
+			mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+			mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+			updateUI();
+		}
+
+		// If the user presses the Start Updates button before GoogleApiClient connects, we set
+		// mRequestingLocationUpdates to true (see startUpdatesButtonHandler()). Here, we check
+		// the value of mRequestingLocationUpdates and if it is true, we start location updates.
+		if (mRequestingLocationUpdates) {
+			startLocationUpdates();
 		}
 	}
 
@@ -245,23 +419,25 @@ public class MainActivity extends Activity implements
 		finish();
 	}
 
+	/**
+	 * Callback that fires when the location changes.
+	 */
 	@Override
 	public void onLocationChanged(Location location) {
-
+		mCurrentLocation = location;
+		mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+		updateUI();
+		Toast.makeText(this, getResources().getString(R.string.location_updated_message),
+				Toast.LENGTH_SHORT).show();
 	}
 
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-
+	/**
+	 * Stores activity data in the Bundle.
+	 */
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
+		savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+		savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+		super.onSaveInstanceState(savedInstanceState);
 	}
 }
--- app/src/main/java/com/asif/shareforcare/app/AppConfig.java
+++ app/src/main/java/com/asif/shareforcare/app/AppConfig.java
@@ -3,8 +3,8 @@ package com.asif.shareforcare.app;
 public class AppConfig {
 	
 	 // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.115/login.php";
+    public static String URL_LOGIN = "http://192.168.1.109/login.php";
  
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.115/register.php";
+    public static String URL_REGISTER = "http://192.168.1.109/register.php";
 }
